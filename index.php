79792c415a810335ca16a81d9843f58ba81cc247
Leo-DIP <80347660+Leo-DIP@users.noreply.github.com>
2021-03-09 17:17:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Click and collect</title>
+
+        <link href="/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+                <h1>Formulaire inscription</h1>
+
+                <form>
+
+  <label for="email">Adresse électronique :</label>
+  <input type="email" id="email" name="email" value="jeandupont@gmail.com">
+  <label for="nom">Nom :</label>
+  <input type="text" id="nom" name="nom" value="Dupont">
+  <label for="prenom">Prénom :</label>
+  <input type="text" id="prenom" name="prenom" value="Jean">
+</form>
+
+    </body>
+</html>
